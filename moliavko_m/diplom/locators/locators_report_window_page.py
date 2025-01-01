978435f5329eaf5d3bc64db4afbdfea82adf44e2
mike <mihailmoliavko@gmail.com>
2025-01-01 23:55:15 +0300

    any text

---
+++ moliavko_m/diplom/locators/locators_report_window_page.py
@@ -0,0 +1,36 @@
+import os
+
+from moliavko_m.diplom.page.base_page import WebPage
+from moliavko_m.diplom.page.elements import WebElement
+
+class ReportWindow(WebPage):
+
+    def __init__(self, web_driver, url=''):
+        if not url:
+            url = os.getenv("MAIN_URL") or 'https://ikea-foundation.gan-compliance.com/p/Case?locale=en-US'
+
+        super().__init__(web_driver, url)
+    ###
+    btn_pip_up_cookies = WebElement(xpath='//button[@class="vogdprc-button vogdprc-button--accept"]')
+    ###
+    select_language = WebElement(xpath='//div [@class="wrapper"]')
+    ###
+    logo = WebElement(xpath='//img [@alt="logo"]')
+    ###
+    txt_main_text = WebElement(xpath='//h1 [@class="gan-typography gan-typography--header1 ng-star-inserted"]')
+    txt_main_text_about = WebElement(xpath='(//p [@class="gan-typography gan-typography--body2 gan-typography--pre-wrap ng-star-inserted"])[1]')
+    txt_contact_info = WebElement(xpath='(//p [@class="gan-typography gan-typography--body2 gan-typography--pre-wrap ng-star-inserted"])[2]')
+    txt_or = WebElement(xpath='//span [@class="esp-access-divider__text ng-star-inserted"]')
+    txt_call_us = WebElement(xpath='//h2 [@class="gan-typography gan-typography--header2 ng-star-inserted"]')
+    txt_powered_by = WebElement(xpath='(//div)[34]')
+    txt_error = WebElement(xpath='//p[@class="gan-typography gan-typography--body2 ng-star-inserted"]')
+    ###
+    btn_create_report = WebElement(xpath='(//div[@class="gan-button__content ng-star-inserted"])[1]')
+    btn_view_case = WebElement(xpath='(//div[@class="gan-button__content ng-star-inserted"])[2]')
+    btn_powered_by = WebElement(xpath='//a[@href="https://ganintegrity.com/privacy-notice/"]')
+    ###
+    input_case_id = WebElement(xpath='(//input)[1]')
+    input_password = WebElement(xpath='(//input)[2]')
+
+
+
---
+++ moliavko_m/diplom/tests_ui/test_report_window_page.py
@@ -0,0 +1,73 @@
+import time
+import allure
+import pytest_check as check
+
+from moliavko_m.diplom.locators.locators_report_window_page import ReportWindow
+from conftest import web_browser
+
+@allure.story('Проверка страницы Report Window')
+@allure.feature('Тест для проверки титульного текста')
+def test_text_report_window(web_browser):
+    page = ReportWindow(web_browser)
+    elements_txt = [
+        (page.txt_main_text, "Welcome to the Foundations’ Trust Line Platform"),
+        (page.txt_main_text_about, " This platform provides a safe and confidential environment to report serious concerns about misconduct, unlawful activities and/ or unethical behaviour, relevant to the IKEA Foundation/ IMAS Foundation/ INGKA Foundation.\n\nWhen reporting a concern, please make sure to note down your Case ID and Password. "),
+        (page.txt_contact_info, " Alternatively, reports can be made over the phone through a hotline.\n\nPlease contact:\nNetherlands 📞 (+31) 0800 0221142\nIndia 📞 000 800 050 3894 "),
+        (page.txt_or, "OR"),
+        (page.txt_call_us, "Call us"),
+        (page.txt_powered_by, "Powered by GAN integrity Inc."),
+    ]
+    for elements, elements_text in elements_txt:
+        with allure.step(""):
+             check.is_true(elements.is_visible)
+             check.equal(elements.get_text(), elements_text)
+
+
+
+@allure.story('Проверка страницы Report Window')
+@allure.feature('Тест для проверки logo')
+def test_logo_report_window(web_browser):
+    page = ReportWindow(web_browser)
+    with allure.step(""):
+         check.is_true(page.logo.is_visible())
+         check.equal(page.logo.get_attribute("width"), "128")
+         check.equal(page.logo.get_attribute("height"), "40")
+
+
+
+@allure.story('Проверка страницы Report Window')
+@allure.feature('Тест для проверки кнопок')
+def test_btn_report_window(web_browser):
+    page = ReportWindow(web_browser)
+    elements_btn = [
+        (page.btn_create_report,"REPORT A CONCERN", ""),
+        (page.btn_view_case, "VIEW CASE", ""),
+        (page.btn_powered_by, "Privacy Notice · Imprint", "https://ganintegrity.com/privacy-notice/"),
+    ]
+    for elements, elements_text, elements_url in elements_btn:
+        with allure.step(""):
+             check.is_true(elements.is_clickable())
+             check.is_true(elements.is_visible())
+             check.equal(elements.get_text(), elements_text)
+
+
+
+@allure.story('Проверка страницы Report Window')
+@allure.feature('Тест для негативной проверки полей ввода')
+def test_input_report_window(web_browser):
+    page = ReportWindow(web_browser)
+    with allure.step(""):
+         page.input_case_id.send_keys("1111111111111111111")
+         page.input_password.send_keys("11111111111111111111")
+         page.btn_view_case.click()
+         check.is_true(page.txt_error.is_visible())
+         check.equal(page.txt_error.get_text(), "Invalid credentials")
+
+
+
+
+
+
+
+
+
